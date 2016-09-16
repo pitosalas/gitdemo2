bc7aeb2110bfa106c93fe2f5cb45f717224d66ec
Pito Salas <pitosalas@gmail.com>
2016-09-16 15:47:30 -0400

    fixed merge weirdness

--- opensesame.rb
+++ opensesame.rb
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 puts "try to make a conflict"
-=======
-<<<<<<< HEAD
 puts "changes"
 puts "Of course it doesnt run!!!"
 puts "but it does"
@@ -10,9 +7,5 @@ puts "but it does"
 puts "Metallica is coming"
 
 puts "MY COMMIT RULES!!!!"
-
-=======
->>>>>>> f2d03ae929fcc4845a3e6fd2b0a47455f42cf149
->>>>>>> cf1a01b9c90b398a488f8d99a269efc260f912e3
 
 puts "MAKE A CONFLICT"
